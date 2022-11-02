d0cf26da67a358efc9249897ca2864e8c526642b
Newton Ombese <newtonombese1@gmail.com>
2022-11-02 12:48:28 +0300

    fixing the advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
-recursive function that queries the Reddit API,
-parses the title of all hot articles, and prints a
-sorted count of given keywords (case-insensitive,
-delimited by spaces
-"""
-
+"""Contains the count_words function"""
 import requests
 
 
 def count_words(subreddit, word_list, found_list=[], after=None):
-    '''return count of keywords in hot posts titles'''
+    '''Prints counts of given words found in hot posts of a given subreddit.
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        found_list (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+    '''
     user_agent = {'User-agent': 'test45'}
     posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
                          .format(subreddit, after), headers=user_agent)
